592ed483e5432d7b7b9d7d527821ccf2612b238c
victor <victorangelograca@hotmail.com>
2020-01-28 18:46:44 -0300

    working?

--- src/config.py
+++ src/config.py
@@ -40,12 +40,12 @@ class Config:
     hunt = "hunt"
 
     red = (255, 0, 0)
-    pink = (255, 128, 128)  # Pink battle list when we're in battle
-    gray = (75, 75, 75)  # Gray battle list in the beginning of monster hp bar
-    black = (255, 255, 255)  # Black battle list when monster hits us
-    green_follow = (104, 246, 104)  # Green color, head, left pixel
-    blue_heal = (63, 108, 154)  # Blue color of heal in middle right before resetting
-    white_cap = (192, 192, 192)  # White color of 0 cap in skills tab
+    pink = (255, 128, 128)
+    gray = (75, 75, 75)
+    black = (255, 255, 255)
+    green_follow = (104, 246, 104)
+    blue_heal = (63, 108, 154)
+    white_cap = (192, 192, 192)
 
     mark = Coord([-1, -1])
     map_begin = Coord([1198, 27])
